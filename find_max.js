0c7f1032173995bbce9ae94225ac4b1be8840804
harshil thummar <hthummar540@gmail.com>
2024-05-28 09:22:04 +0530

    alll done

--- find_max.js
+++ find_max.js
@@ -10,7 +10,7 @@ console.log(max);
 let arr1 = [90, 230, 303, 5003, 230];
 let min = arr1[0];
 
-for (let index = 0; index < arr1.length; index++) {
+for   (let index = 0; index < arr1.length; index++) {
   if (arr1[index] < min) {
     min = arr1[index];
   }
