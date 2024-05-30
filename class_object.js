37be7cfbed9717852998e752c7a1d5d740383b8c
harshil thummar <hthummar540@gmail.com>
2024-05-30 21:37:34 +0530

    "Updated calculator.html with minor styling changes and added a name header. Also, modified string_method.js to include additional string operations."

---
+++ class_object.js
@@ -0,0 +1,10 @@
+// all power within you you can do anything and everything  -- Swami Vivekanand
+
+const StudentDetails = {
+  FullName: "Harshi Thummar",
+  age: 19,
+  developer: true,
+  printage: function() {
+    console.log("age = "+ this.age);
+  },
+};
---
+++ string_method.js
@@ -0,0 +1,5 @@
+let bio = 'my name is harshil'
+length = bio.length;
+console.log(length);
+ch = bio.charAt(4);
+console.log(ch);
