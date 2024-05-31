c26c3af08b0e55f018d5529d1897d0935666083f
harshil thummar <hthummar540@gmail.com>
2024-05-31 11:02:01 +0530

    done

--- string_method.js
+++ string_method.js
@@ -1,5 +1,14 @@
-let bio = 'my name is harshil'
+let bio = "my name is harshil";
 length = bio.length;
 console.log(length);
 ch = bio.charAt(4);
 console.log(ch);
+
+ str = "my name is harshil thummar";
+ strnfh = str.length
+ console.log(strnfh);
+size = str.length;
+console.log(size);
+ let join = bio.concat(str);
+console.log(join);
+let joi_n = str.concat(bio)
